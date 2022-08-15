ef3b0cc1f311c40212fc44dc35a82f5f5e9490dc
ganeshsuryawansh <96677595+ganeshsuryawansh@users.noreply.github.com>
2022-08-15 22:34:22 +0530

    message form addition

---
+++ JS/alert.js
@@ -0,0 +1,10 @@
+
+console.log("hii i am js");
+
+function showalert(){
+    console.log("i am inside alert function");
+
+    let name=document.form1.name.value;
+    alert("Dear "+name+" Your Message is Succsfully Send To Ganesh Suryawanshi! Welcome This Portal");
+
+}
