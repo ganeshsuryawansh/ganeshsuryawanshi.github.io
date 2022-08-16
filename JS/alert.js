6deae5d86d7204842ebaf925f89d3065d1c2f5bb
ganeshsuryawansh <96677595+ganeshsuryawansh@users.noreply.github.com>
2022-08-16 09:03:21 +0530

    form edit

--- JS/alert.js
+++ JS/alert.js
@@ -3,8 +3,11 @@ console.log("hii i am js");
 
 function showalert(){
     console.log("i am inside alert function");
-
     let name=document.form1.name.value;
-    alert("Dear "+name+" Your Message is Succsfully Send To Ganesh Suryawanshi! Welcome This Portal");
-
+    if(name===""){
+        alert("Please Enter Your Name And Email");
+    } 
+    else{
+        alert("Dear "+name+" Your Message is Succsfully Send To Ganesh Suryawanshi! Welcome This Portal");
+    }
 }
